49f212ecda8e3b0beb197069c7aa8dbc70fda943
celyes <celyes02@gmail.com>
2020-01-18 16:41:11 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Navbar from './Navbar.svelte';
+	let name = "ilyes";
+</script>
+
+<main>
+	<Navbar></Navbar>
+	<h1>Hello {name}!</h1>	
+</main>
+
+<style>
+	main {
+		text-align: center;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Navbar.svelte
@@ -0,0 +1,45 @@
+<script>
+ import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+  } from 'sveltestrap';
+
+  let isOpen = false;
+
+  function handleUpdate(event) {
+    isOpen = event.detail.isOpen;
+  }
+</script>
+
+<Navbar color="light" light expand="md">
+  <NavbarBrand href="/">Ilyes</NavbarBrand>
+  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+    <Nav class="ml-auto" navbar>
+      <NavItem>
+        <NavLink href="#components/">Components</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="https://github.com/bestguy/sveltestrap">GitHub</NavLink>
+      </NavItem>
+      <UncontrolledDropdown nav inNavbar>
+        <DropdownToggle nav caret>Options</DropdownToggle>
+        <DropdownMenu right>
+          <DropdownItem>Option 1</DropdownItem>
+          <DropdownItem>Option 2</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Reset</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    </Nav>
+  </Collapse>
+</Navbar>
